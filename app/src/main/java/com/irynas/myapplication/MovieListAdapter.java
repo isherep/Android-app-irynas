e31791c838042a9c512629504b67d4d8c1353d86
Iryna <iryna.sherepot@gmail.com>
2019-04-29 22:11:36 -0700

    updaged movie adapter - working list

--- app/src/main/java/com/irynas/myapplication/MovieListAdapter.java
+++ app/src/main/java/com/irynas/myapplication/MovieListAdapter.java
@@ -1,42 +1,38 @@
 package com.irynas.myapplication;
 
 import android.content.Context;
+import android.net.Uri;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
 public class MovieListAdapter extends ArrayAdapter<String[]> {
-    private final Context context;
-    private final String[][] movies;
 
     public MovieListAdapter(Context context, String[][] movies) {
-        super(context, -1);
-        this.context = context;
-        this.movies = movies;
+        super(context, -1, movies);
     }
 
+    @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        LayoutInflater inflater = (LayoutInflater) context
-                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        View rowView = inflater.inflate(R.layout.movie_list_row_item, parent, false);
+        if (convertView == null) {
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.movie_list_row_item, parent, false);
+        }
 
-        TextView movieTitle = (TextView) rowView.findViewById(R.id.movieTitle);
-        movieTitle.setText(movies[position][0]);
 
-//        LinearLayout layout = new LinearLayout(context);
-//        layout.setOrientation(LinearLayout.VERTICAL);
-//
-//        TextView title = new TextView(context);
-//        title.setText(movies[position][0]);
-//
-//        TextView id = new TextView(context);
-//        id.setText("ID: " + position);
-//
-//        layout.addView(title);
-//        layout.addView(id);
-        return rowView;
+        String[] movie = getItem(position);
+        TextView movieTitle = (TextView) convertView.findViewById(R.id.movieTitle);
+        movieTitle.setText(movie[0]);
+
+        TextView movieYear = (TextView) convertView.findViewById(R.id.movieYear);
+        movieYear.setText(movie[1]);
+
+        ImageView movieImage = (ImageView) convertView.findViewById(R.id.movieImage);
+        movieImage.setImageURI(Uri.parse(movie[3]));
+
+        return convertView;
     }
 }
